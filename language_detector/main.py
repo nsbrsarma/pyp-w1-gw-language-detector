851d69860542f6e6961a34b3b9f19310dc6157b4
Sharma Nadakuditi <nsbrsarma@hotmail.com>
2016-08-25 22:01:57 +0000

    Update py ver

--- language_detector/main.py
+++ language_detector/main.py
@@ -2,7 +2,7 @@
 
 """This is the entry point of the program."""
 
-from languages import LANGUAGES
+from .languages import LANGUAGES
 
 
 def detect_language(text, languages=LANGUAGES):
--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     packages=['language_detector'],
     maintainer='rmotr.com',
     tests_require=[
-        'pytest==2.9.1',
+        'pytest==3.0.1',
         'pytest-cov==2.2.1',
         'coverage==4.0.3'
     ],
